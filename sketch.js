f0b7dc975ab06513858407889fc39c86243f8978
Stone Mathers <smm117@duke.edu>
2019-01-30 00:46:36 -0500

    Refactored to a Letter class

--- sketch.js
+++ sketch.js
@@ -4,15 +4,8 @@ const CANVAS_HEIGHT = 650;
 const BGCOLOR_DIV = 40;
 const BG_COLOR = "#000";
 
-//Variables for letters
+//Array of letters
 let letters = [];
-let fades = [];
-let directions = [];
-let sizes = [];
-let speeds = [];
-let colors = [];
-let locsX = [];
-let locsY = [];
 
 //Letter Constants
 const COLOR_INCR = 160;
@@ -36,158 +29,34 @@ function setup(){
 }
 
 function draw(){
-    let numLet = letters.length;
-
     //Draw background
     background(BG_COLOR);
 
     //Draw letters
     for(i = 0; i < letters.length; i++){
-        let fade = fades[i];
-        if(fade > 0){
-            let letter = letters[i];
-            let size = sizes[i];
-            let x = locsX[i];
-            let y = locsY[i];
-            let dir = directions[i];
-            let speed = speeds[i];
-
-            textSize(size);
-            fill(lerpColor(color(BG_COLOR), colors[i], fade/COLOR_INCR));
-
-            if(dir == DIAG_UL){
-                //Draw line of letters
-                //Draw letters up-left of starter
-                tempX = x - size;
-                tempY = y - size;
-                while(tempX > -size && tempY > -size){
-                    text(letter, tempX, tempY);
-                    tempX -= size;
-                    tempY -= size;
-                }
-
-                //Draw starter
-                text(letter, x, y);
-
-                //Draw letters down-right of starter
-                tempX = x + size;
-                tempY = y + size;
-                while(tempX < CANVAS_WIDTH && tempY < CANVAS_HEIGHT){
-                    text(letter, tempX, tempY);
-                    tempX += size;
-                    tempY += size;
-                }
-
-                //Update position
-                locsX[i] = x - speed;
-                locsY[i] = y - speed;
-            }else if(dir == DIAG_DL){
-                //Draw line of letters
-                //Draw letters down-left of starter
-                tempX = x - size;
-                tempY = y + size;
-                while(tempX > -size && tempY < CANVAS_HEIGHT){
-                    text(letter, tempX, tempY);
-                    tempX -= size;
-                    tempY += size;
-                }
-
-                //Draw starter
-                text(letter, x, y);
-
-                //Draw letters up-right of starter
-                tempX = x + size;
-                tempY = y - size;
-                while(tempX < CANVAS_WIDTH && tempY > -size){
-                    text(letter, tempX, tempY);
-                    tempX += size;
-                    tempY -= size;
-                }
-
-                //Update position
-                locsX[i] = x - speed;
-                locsY[i] = y + speed;
-            }else if(dir == DIAG_DR){
-                //Draw line of letters
-                //Draw letters up-left of starter
-                tempX = x - size;
-                tempY = y - size;
-                while(tempX > -size && tempY > -size){
-                    text(letter, tempX, tempY);
-                    tempX -= size;
-                    tempY -= size;
-                }
-
-                //Draw starter
-                text(letter, x, y);
-
-                //Draw letters down-right of starter
-                tempX = x + size;
-                tempY = y + size;
-                while(tempX < CANVAS_WIDTH && tempY < CANVAS_HEIGHT){
-                    text(letter, tempX, tempY);
-                    tempX += size;
-                    tempY += size;
-                }
-
-                //Update position
-                locsX[i] = x + speed;
-                locsY[i] = y + speed;
-            }else if(dir == DIAG_UR){
-                //Draw line of letters
-                //Draw letters down-left of starter
-                tempX = x - size;
-                tempY = y + size;
-                while(tempX > -size && tempY < CANVAS_HEIGHT){
-                    text(letter, tempX, tempY);
-                    tempX -= size;
-                    tempY += size;
-                }
-
-                //Draw starter
-                text(letter, x, y);
-
-                //Draw letters up-right of starter
-                tempX = x + size;
-                tempY = y - size;
-                while(tempX < CANVAS_WIDTH && tempY > -size){
-                    text(letter, tempX, tempY);
-                    tempX += size;
-                    tempY -= size;
-                }
-
-                //Update position
-                locsX[i] = x + speed;
-                locsY[i] = y - speed;
-            }
-
-            //Decrement fade value
-            fades[i] = fade - 1;
-        }else{
+        let currLet = letters[i];
+        if(currLet.faded()){
             //Remove faded letter
             letters.splice(i, 1);
-            fades.splice(i, 1);
-            directions.splice(i, 1);
-            sizes.splice(i, 1);
-            speeds.splice(i, 1);
-            colors.splice(i, 1);
-            locsX.splice(i, 1);
-            locsY.splice(i, 1);
             i--;
+        }else{
+            currLet.drawAsLine();
+            currLet.move();
+            currLet.fade(1);
         }
     }
 }
 
 function keyPressed(){
     if(key.length == 1){
-        letters.push(key);
-        fades.push(COLOR_INCR);
-        directions.push(getRandomInt(0, DIR_MAX));
-        sizes.push(getRandomInt(SIZE_MIN, SIZE_MAX));
-        speeds.push(getRandomInt(SPEED_MIN, SPEED_MAX));
-        colors.push(color(COLOR_PALLETE[getRandomInt(0, COLOR_PALLETE.length - 1)]));
-        locsX.push(getRandomInt(0, CANVAS_WIDTH));
-        locsY.push(getRandomInt(0, CANVAS_HEIGHT));
+        let dir = getRandomInt(0, DIR_MAX);
+        let sz = getRandomInt(SIZE_MIN, SIZE_MAX);
+        let sp = getRandomInt(SPEED_MIN, SPEED_MAX);
+        let clr = color(COLOR_PALLETE[getRandomInt(0, COLOR_PALLETE.length - 1)]);
+        let x = getRandomInt(0, CANVAS_WIDTH);
+        let y = getRandomInt(0, CANVAS_HEIGHT);
+        let newLetter = new Letter(key, COLOR_INCR, dir, sz, sp, clr, x, y);
+        letters.push(newLetter);
     }
 }
 
@@ -196,4 +65,123 @@ function keyPressed(){
 */
 function getRandomInt(min, max){
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+/*
+* Class for Letter object
+*/
+class Letter{
+    constructor(letter, fadeRemaining, direction, size, speed, color, x, y){
+        this.letter = letter;
+        this.fadeRemaining = fadeRemaining;
+        this.direction = direction;
+        this.size = size;
+        this.speed = speed;
+        this.color = color;
+        this.x = x;
+        this.y = y;
+    }
+
+    /*
+    * Draw letter at given position
+    */
+    draw(){
+        textSize(this.size);
+        fill(lerpColor(color(BG_COLOR), this.color, this.fadeRemaining/COLOR_INCR));
+        text(this.letter, this.x, this.y);
+    }
+
+    /*
+    * Move letter according to speed and direction
+    */
+    move(){
+        if(this.direction == DIAG_UL){
+            this.x -= this.speed;
+            this.y -= this.speed;
+        }else if(this.direction == DIAG_DL){
+            this.x -= this.speed;
+            this.y += this.speed;
+        }else if(this.direction == DIAG_DR){
+            this.x += this.speed;
+            this.y += this.speed;
+        }else if(this.direction == DIAG_UR){
+            this.x += this.speed;
+            this.y -= this.speed;
+        }else{
+            //Error - should never reach here
+        }
+    }
+
+    /*
+    * Draw several instances of letter in a line according to direction
+    */
+    drawAsLine(){
+        //Draw base letter - also sets fill and text size
+        this.draw();
+
+        if(this.direction == DIAG_UL || this.direction == DIAG_DR){
+            let tempX, tempY;
+            
+            //Draw letters up-left of starter
+            tempX = this.x - this.size;
+            tempY = this.y - this.size;
+            while(tempX > -this.size && tempY > 0){
+                text(this.letter, tempX, tempY);
+                tempX -= this.size;
+                tempY -= this.size;
+            }
+
+            //Draw letters down-right of starter
+            tempX = this.x + this.size;
+            tempY = this.y + this.size;
+            while(tempX < CANVAS_WIDTH && tempY < CANVAS_HEIGHT + this.size){
+                text(this.letter, tempX, tempY);
+                tempX += this.size;
+                tempY += this.size;
+            }
+            
+        }else if(this.direction == DIAG_DL || this.direction == DIAG_UR){
+            let tempX, tempY;
+
+            //Draw letters down-left of starter
+            tempX = this.x - this.size;
+            tempY = this.y + this.size;
+            while(tempX > -this.size && tempY < CANVAS_HEIGHT + this.size){
+                text(this.letter, tempX, tempY);
+                tempX -= this.size;
+                tempY += this.size;
+            }
+
+            //Draw letters up-right of starter
+            tempX = this.x + this.size;
+            tempY = this.y - this.size;
+            while(tempX < CANVAS_WIDTH && tempY > 0){
+                text(this.letter, tempX, tempY);
+                tempX += this.size;
+                tempY -= this.size;
+            }
+        }else{
+            //Error - should never reach here
+        }
+    }
+
+    /*
+    * Decrease amount of fade remaining
+    *
+    * Input:
+    *   fadeAmount - integer amount to decrease fade
+    */
+    fade(fadeAmount){
+        this.fadeRemaining -= fadeAmount;
+    }
+
+    /*
+    * Determine whether or not letter is faded
+    *
+    * Output:
+    *   true if letter is faded, false if not
+    */
+    faded(){
+        return this.fadeRemaining <= 0;
+    }
 }
